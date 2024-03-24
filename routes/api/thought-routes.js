6d4f45c37a2752fbb5247b6c2e2edbeb0cdb3449
ThatGiodude <giovannifstrangio@gmail.com>
2024-03-24 18:09:46 -0400

    fixed errors and pathing after several crashes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getAllThoughts).post(createThought);
+
+router.route('/:id').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions/')
+    .post(addReaction).delete(deleteReaction);
+    
+
+module.exports = router;
